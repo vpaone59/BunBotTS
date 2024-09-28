fd4f43d6af5f4750fe3c652d3b3967270157745c
vinny <paone@rowan.edu>
2024-09-28 15:49:45 -0400

    added utility files for commands

--- index.ts
+++ index.ts
@@ -1,11 +1,15 @@
 console.log("Hello via Bun!");
 
 // Require the necessary discord.js classes
-const { Client, Events, GatewayIntentBits } = require('discord.js');
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.commands = new Collection();
 
 // When the client is ready, run this code (only once).
 // The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
